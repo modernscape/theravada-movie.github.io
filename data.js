339e3c1fee3c1124e6dd625e21988057a524bcbf
modernscape <modernscape@gmail.com>
2021-10-15 11:48:05 +0900

    21-10-15

---
+++ data.js
@@ -0,0 +1,33 @@
+var exItems = [
+  {
+    id: 0,
+    imageUrl: 'https://i.ytimg.com/vi/b6XNs4i10JU/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCPGYynv4MIs30ZXQZKifmzt25zAQ',
+    movieUrl:'https://youtu.be/b6XNs4i10JU',
+    title: '死に支度（しにじたく）のススメ？（22 Aug 2021 ゴータミー精舎）',
+    done: false,
+  }, 
+  {
+    id: 1,
+    imageUrl: 'https://i.ytimg.com/vi/EY4VkgRgOpM/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBNgexXWczfRdzgjwqhfHiyT7PW5A',
+    movieUrl:'https://youtu.be/EY4VkgRgOpM?list=PLv-Wl8PVb-JDqDYXfRYRhmz_1fG25GTjO',
+    title: '正見で学ぶ「仕事」の意味ーーブッダになっても仕事からは逃げられない｜スマナサーラ長老の初期仏教テーマトーク（26Sep 2021 ゴータミー精舎）',
+    done: true,
+  },
+  {
+    id: 2,
+    imageUrl: 'https://i.ytimg.com/vi/QrxMgTTDuWA/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA-MRzONjlPzMhTjhR5GI5JRdL11g',
+    movieUrl:'https://youtu.be/QrxMgTTDuWA?list=PLv-Wl8PVb-JBtqjEdjnElDBXO8DFknK0T',
+    title: '”Depressed” 混乱した社会のなか、どう生きる？｜スマナサーラ長老の初期仏教テーマトーク（29 Aug 2021)ゴータミー精舎',
+    done: false,
+  },
+  {
+    id: 3,
+    imageUrl: 'https://i.ytimg.com/vi/fCBfXPRZqkg/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAR36_iLGDFOcpwV0Qoj-JSBH2_4w',
+    movieUrl:'https://youtu.be/yIYjoOOU04s?list=PLv-Wl8PVb-JBtqjEdjnElDBXO8DFknK0T',
+    title: '無常の見方　2021｜スマナサーラ長老の初期仏教テーマトーク（05 Sep 2021 ゴータミー精舎）',
+    done: false,
+  }
+];
+
+
+
