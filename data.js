e5c14d90ff777971f23f59a189103f431d52716d
modernscape <modernscape@gmail.com>
2021-11-07 12:44:09 +0900

    21-11-07_

--- data.js
+++ data.js
@@ -1,26 +1,26 @@
 var exItems = [
   {
-    imageUrl: 'https://i.ytimg.com/vi/b6XNs4i10JU/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCPGYynv4MIs30ZXQZKifmzt25zAQ',
     movieUrl:'https://youtu.be/b6XNs4i10JU',
+    imageUrl: 'https://i.ytimg.com/vi/b6XNs4i10JU/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCPGYynv4MIs30ZXQZKifmzt25zAQ',
     title: '死に支度（しにじたく）のススメ？（22 Aug 2021 ゴータミー精舎）',
     done: true,
   }, 
   {
-    imageUrl: 'https://i.ytimg.com/vi/EY4VkgRgOpM/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBNgexXWczfRdzgjwqhfHiyT7PW5A',
     movieUrl:'https://youtu.be/EY4VkgRgOpM?list=PLv-Wl8PVb-JDqDYXfRYRhmz_1fG25GTjO',
+    imageUrl: 'https://i.ytimg.com/vi/EY4VkgRgOpM/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBNgexXWczfRdzgjwqhfHiyT7PW5A',
     title: '正見で学ぶ「仕事」の意味ーーブッダになっても仕事からは逃げられない｜スマナサーラ長老の初期仏教テーマトーク（26Sep 2021 ゴータミー精舎）',
     done: true,
   },
   {
-    imageUrl: 'https://i.ytimg.com/vi/QrxMgTTDuWA/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA-MRzONjlPzMhTjhR5GI5JRdL11g',
     movieUrl:'https://youtu.be/QrxMgTTDuWA?list=PLv-Wl8PVb-JBtqjEdjnElDBXO8DFknK0T',
+    imageUrl: 'https://i.ytimg.com/vi/QrxMgTTDuWA/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA-MRzONjlPzMhTjhR5GI5JRdL11g',
     title: '”Depressed” 混乱した社会のなか、どう生きる？｜スマナサーラ長老の初期仏教テーマトーク（29 Aug 2021)ゴータミー精舎',
     done: true,
   },
   {
     id: 3,
-    imageUrl: 'https://i.ytimg.com/vi/fCBfXPRZqkg/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAR36_iLGDFOcpwV0Qoj-JSBH2_4w',
     movieUrl:'https://youtu.be/yIYjoOOU04s?list=PLv-Wl8PVb-JBtqjEdjnElDBXO8DFknK0T',
+    imageUrl: 'https://i.ytimg.com/vi/fCBfXPRZqkg/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAR36_iLGDFOcpwV0Qoj-JSBH2_4w',
     title: '無常の見方　2021｜スマナサーラ長老の初期仏教テーマトーク（05 Sep 2021 ゴータミー精舎）',
     done: false,
   },
@@ -86,7 +86,7 @@ var exItems = [
   }, 
   {
     movieUrl:'https://youtu.be/gjLFEnuXnyg',
-    imageUrl: '',
+    imageUrl: 'https://i.ytimg.com/vi/NWy9hcMh9sM/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLAAYKpdeD9u4AXMD_xjspaFfa5Wdg',
     title: '嫉妬しないこと',
     done: false,
   }
